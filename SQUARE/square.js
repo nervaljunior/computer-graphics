8b9ce891ab1cf7fc1618dcbb1a52c8fd6d89d915
nervaljunior <nerval.junior@discente.ufma.br>
2023-05-14 23:33:16 -0300

    onclick de delauney

--- SQUARE/square.js
+++ SQUARE/square.js
@@ -15,49 +15,43 @@ var colors = [
 
 window.onload = function init() {
   canvas = document.getElementById("gl-canvas");
+  
   generate = document.getElementById("generate");
-  delaunay = document.getElementById("delaunay");
+  delau = document.getElementById("delaunay");
   Voronoi = document.getElementById("Voronoi");
   clear = document.getElementById("clear");
   vertices = document.getElementById("vertices");
 
-  delaunay.addEventListener("click", function(event) {
-// Criar conjunto aleatório de pontos
-// Criar conjunto aleatório de pontos
-/*
-const n = 50;
-const points = Array.from({length: n}, () => [Math.random() * 500, Math.random() * 500]);
-*/
-// Fazer triangulação de Delaunay
-const delaunay = d3.Delaunay.from(point);
-
-// Plotar triangulação de Delaunay
-const context = canvas.getContext("2d");
-
-context.strokeStyle = "black";
-context.beginPath();
-delaunay.render(context);
-context.stroke();
-
-/*
-const points = Array.from({length: point}, () => [Math.random() * 500, Math.random() * 500]);
-
-// Fazer triangulação de Delaunay
-const delaunay = d3.Delaunay.from(points);
-
-// Plotar triangulação de Delaunay
-const svg = d3.select("svg");
-svg.selectAll("path")
-  .data(delaunay.trianglePolygons())
-  .join("path")
-    .attr("d", polygon => "M" + polygon.join("L") + "Z")
-    .attr("fill", "none")
-    .attr("stroke", "black");*/
-
-  });  
-  Voronoi.addEventListener("click", function(event) {
-
-  });  
+  delau.addEventListener("click", function(event) {
+    const points = point.slice(0, length_point);
+    const delaunay = d3.Delaunay.from(points);
+    const context = canvas.getContext("webgl");
+    context.clearColor(0.5, 0.5, 0.5, 1.0);
+    context.clear(context.COLOR_BUFFER_BIT);
+    const triangles = delaunay.triangleArray();
+    const edges = new Set();
+    for (let i = 0; i < triangles.length; i += 3) {
+      edges.add(`${triangles[i]}-${triangles[i+1]}`);
+      edges.add(`${triangles[i+1]}-${triangles[i+2]}`);
+      edges.add(`${triangles[i+2]}-${triangles[i]}`);
+    }
+    const positions = points.flatMap(p => [p[0], p[1]]);
+    const indices = Array.from(edges.values()).flatMap(edge => {
+      const [i, j] = edge.split("-").map(Number);
+      return [i, j];
+    });
+    const buffer = context.createBuffer();
+    context.bindBuffer(context.ARRAY_BUFFER, buffer);
+    context.bufferData(context.ARRAY_BUFFER, new Float32Array(positions), context.STATIC_DRAW);
+    const positionLoc = context.getAttribLocation(context.program, "vPosition"); // <-- Use context instead of gl
+    context.vertexAttribPointer(positionLoc, 2, context.FLOAT, false, 0, 0);
+    context.enableVertexAttribArray(positionLoc);
+    const indexBuffer = context.createBuffer();
+    context.bindBuffer(context.ELEMENT_ARRAY_BUFFER, indexBuffer);
+    context.bufferData(context.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), context.STATIC_DRAW);
+    context.drawElements(context.LINES, indices.length, context.UNSIGNED_SHORT, 0);
+  });
+  
 
   gl = WebGLUtils.setupWebGL(canvas);
   if (!gl) {
@@ -108,7 +102,6 @@ svg.selectAll("path")
   clear.addEventListener("click", function (event) {
     location.reload();
   });
-  
     
   vertices.addEventListener("click", function (event) {
     console.log(convexHull1(point, length_point));
@@ -195,7 +188,6 @@ svg.selectAll("path")
     </table>
   `;
   document.write(tabela);
-  
   }
 };
 
